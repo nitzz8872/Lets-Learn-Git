90f6982e77880cb2e748a5b0ee3e880ff0468d46
Manzar Mustafa <60620214+manzarmustafa@users.noreply.github.com>
2023-10-20 12:00:29 +0530

    Update Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,7 +1,7 @@
 public class Multiply {
     public static void main(String Args[]){
         int a=2,b=4;
-        int d=a*b;
-        System.out.println("The product is "+d);
+        int m=a*b;
+        System.out.println("The product is "+m);
     }
 }
