6b605eb8ddd4a621c6279e35f0d7ec0c8ca2d813
Ritika-Raj-Jaiswal <85188568+Ritika-Raj-Jaiswal@users.noreply.github.com>
2023-10-25 12:05:28 +0530

    Update Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,7 +1,7 @@
 public class Multiply {
     public static void main(String Args[]){
         int a=6,b=3;
-        int n=a*b;
-        System.out.println("The product is "+n);
+        int z=a*b;
+        System.out.println("The product is "+z);
     }
 }
